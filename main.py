6852a14ae2300c251f6cb7b9b2dcca6afc630948
Lev-Janicki <72770454+Lev-Janicki@users.noreply.github.com>
2020-11-17 11:11:08 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 x = input.temperature(TemperatureUnit.FAHRENHEIT)
+y = input.temperature(TemperatureUnit.CELSIUS)
 while True:
-    print("Temperature is (F): " + input.temperature(TemperatureUnit.FAHRENHEIT))
-    if x < 75:   
+    print("Temperature is (F): " +x)
+    print("Temperature(C): " +y)
+    if x < 75:  
         light.show_animation(light.rainbowAnimation, 500)
--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 let x = input.temperature(TemperatureUnit.Fahrenheit)
+let y = input.temperature(TemperatureUnit.Celsius)
 while (true) {
-    console.log("Temperature is (F): " + input.temperature(TemperatureUnit.Fahrenheit))
+    console.log("Temperature is (F): " + x)
+    console.log("Temperature(C): " + y)
     if (x < 75) {
         light.showAnimation(light.rainbowAnimation, 500)
     }
