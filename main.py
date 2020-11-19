2068b886a372769de9dcb3885d413d8a4791dd34
Lev-Janicki <72770454+Lev-Janicki@users.noreply.github.com>
2020-11-19 12:48:33 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -2,5 +2,9 @@ x = input.temperature(TemperatureUnit.FAHRENHEIT)
 y = input.temperature(TemperatureUnit.CELSIUS)
 while True:
     print("Temperature is: " +x+"F - " +y+"C")
-    if x < 75:  
-        light.show_animation(light.rainbowAnimation, 500)
+    if x > 70:  
+        light.set_all(light.rgb(255,0,0))
+    elif x<70 and x>40:
+        light.set_all(light.rgb(0,255,0))
+    else:
+        light.set_all(light.rgb(0,0,255))
--- main.ts
+++ main.ts
@@ -2,8 +2,12 @@ let x = input.temperature(TemperatureUnit.Fahrenheit)
 let y = input.temperature(TemperatureUnit.Celsius)
 while (true) {
     console.log("Temperature is: " + x + "F - " + y + "C")
-    if (x < 75) {
-        light.showAnimation(light.rainbowAnimation, 500)
+    if (x > 70) {
+        light.setAll(light.rgb(255, 0, 0))
+    } else if (x < 70 && x > 40) {
+        light.setAll(light.rgb(0, 255, 0))
+    } else {
+        light.setAll(light.rgb(0, 0, 255))
     }
     
 }
