1e5dea7b9ff7fe0fc110b8ed7dd70bc085cecc4e
Lev-Janicki <72770454+Lev-Janicki@users.noreply.github.com>
2020-11-30 18:01:14 -0500

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-light.set_brightness(5)
-#x = input.temperature(TemperatureUnit.FAHRENHEIT)
-#y = input.temperature(TemperatureUnit.CELSIUS)
 while True:
-    print("Temperature is: " +input.temperature(TemperatureUnit.FAHRENHEIT)+"F  ") #+y+"C")
-    if input.temperature(TemperatureUnit.FAHRENHEIT) > 70:  
-        light.set_all(light.rgb(0,0,255))
-    elif input.temperature(TemperatureUnit.FAHRENHEIT)>40:
-        light.set_all(light.rgb(0,255,0))
-    else:
+    print("Temperature is: " +input.temperature(TemperatureUnit.FAHRENHEIT)+"F  ") 
+    if input.temperature(TemperatureUnit.FAHRENHEIT) > 72:  
         light.set_all(light.rgb(255,0,0))
+        pause(500)
+        light.clear()
+        pause(500)
+    elif input.temperature(TemperatureUnit.FAHRENHEIT)<70:
+        light.set_all(light.rgb(0,0,255))
+        pause(500)
+        def on_forever():
+            pause(500)
+        forever(on_forever)
+    else:
+        light.set_all(light.rgb(0,255,0))
--- main.ts
+++ main.ts
@@ -1,15 +1,18 @@
-light.setBrightness(5)
-// x = input.temperature(TemperatureUnit.FAHRENHEIT)
-// y = input.temperature(TemperatureUnit.CELSIUS)
 while (true) {
     console.log("Temperature is: " + input.temperature(TemperatureUnit.Fahrenheit) + "F  ")
-    // +y+"C")
-    if (input.temperature(TemperatureUnit.Fahrenheit) > 70) {
-        light.setAll(light.rgb(0, 0, 255))
-    } else if (input.temperature(TemperatureUnit.Fahrenheit) > 40) {
-        light.setAll(light.rgb(0, 255, 0))
-    } else {
+    if (input.temperature(TemperatureUnit.Fahrenheit) > 72) {
         light.setAll(light.rgb(255, 0, 0))
+        pause(500)
+        light.clear()
+        pause(500)
+    } else if (input.temperature(TemperatureUnit.Fahrenheit) < 70) {
+        light.setAll(light.rgb(0, 0, 255))
+        pause(500)
+        forever(function on_forever() {
+            pause(500)
+        })
+    } else {
+        light.setAll(light.rgb(0, 255, 0))
     }
     
 }
